686c39b07122ced4159bfeaabe0f843275d1094d
nattybear <nattybear@naver.com>
2017-04-11 02:58:09 +0900

    change how query apply

--- news.py
+++ news.py
@@ -1,20 +1,11 @@
 from bottle import run, route, template, debug
 from sqlite3 import connect
 
-query = '''
-SELECT headline, url, time from boannews
-UNION
-SELECT headline, url, time from dailysecu
-UNION
-SELECT headline, url, time from etnews
-ORDER BY time DESC
-'''
-
 @route('/news')
 def news_list():
 	con = connect('news.db')
 	c = con.cursor()
-	c.execute(query)
+	c.execute(open('query.conf', 'rb').read())
 	result = c.fetchall()
 	c.close()
 	output = template('make_table', rows=result)
