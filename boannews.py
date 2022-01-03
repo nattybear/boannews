243824dd913ecefb0d5ff9201268ceb9ce9d9775
준규 <vpark45@gmail.com>
2022-01-03 02:27:01 +0000

    Create boannews.py and schema.sql

---
+++ boannews.py
@@ -0,0 +1,25 @@
+import sqlite3
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = 'http://www.boannews.com/media/news_rss.xml?skind=5'
+with urllib.request.urlopen(url) as f:
+  s = f.read().decode('euc-kr')
+root = ET.fromstring(s)
+channel = root[0]
+items = filter(lambda x: x.tag == 'item', channel)
+con = sqlite3.connect('boannews.db')
+cur = con.cursor()
+sql = 'INSERT INTO boannews VALUES (?, ?, DATETIME(), FALSE)'
+
+for item in items:
+  title = item[0].text.strip()
+  url = item[1].text
+  t = title, url
+  try:
+    cur.execute(sql, t)
+    con.commit()
+  except sqlite3.IntegrityError:
+    pass
+
+con.close()
---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE boannews (
+  url   TEXT PRIMARY KEY,
+  title TEXT,
+  date  TEXT,
+  sent  BOOLEAN
+);
